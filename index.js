3cb95e1c4e8f79a94bfaaf90210c2b32b1e9a9ba
Amrit Raj <77192710+amritmaurya1504@users.noreply.github.com>
2021-10-03 18:54:25 +0530

    Open accordian function added

---
+++ index.js
@@ -0,0 +1,7 @@
+let accordion = document.getElementsByClassName('contentBx');
+
+        for(i = 0; i< accordion.length; i++){
+            accordion[i].addEventListener('click' , function(){
+              this.classList.toggle('active')
+            });
+        }
